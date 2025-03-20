b39025e44d0cf5c8cea0f096d4927b58ead61845
Sara Vieira <araujo.svieira1983@gmail.com>
2025-03-20 16:34:03 -0300

    Add files via upload

---
+++ Sintaxe Básica com Python/SistemadeBanco.py
@@ -0,0 +1,82 @@
+saldo = 1000  # Saldo inicial
+extrato = {}  # Dicionário para armazenar os depósitos
+contador_transacoes = 1  # Contador para organizar os depósitos no dicionário
+LIMITE_SAQUES =3
+
+def exibir_menu(): #menu
+    """Exibe o menu de opções para o usuário."""
+    print("\nMenu:")
+    print("1. Depositar")
+    print("2. Sacar")
+    print("3. Extrato")
+    print("4. Sair")
+
+def deposito():
+    """Realiza um depósito e armazena no extrato."""
+    global saldo, contador_transacoes  # Permite modificar as variáveis globais dentro da função
+    
+    try: # tratar  exceções
+        valor = float(input("Digite o valor do depósito: "))
+        if valor > 0:
+            saldo += valor  # Atualiza o saldo
+            extrato[f"Depósito {contador_transacoes}"] = f"+ R$ {valor:.2f}"  # Armazena no dicionário
+            contador_transacoes += 1  # Incrementa o contador de transações
+            
+            print(f"O depósito de R$ {valor:.2f} foi realizado com sucesso!")
+            print(f"Saldo atualizado: R$ {saldo:.2f}")
+        else:
+            print("Erro: O valor precisa ser positivo.")
+    except ValueError: #se ocorrer algum erro
+        print("Erro: Digite um número válido.")
+
+
+def sacar ():
+    global saldo, contador_transacoes ,LIMITE_SAQUES
+    try: # tratar  exceções 
+        valor =float(input('Digite o valor do saque :'))
+        if  valor >  saldo : #Valor não pode ser maior que o saldo
+            print ('Valor insulficiente')
+        elif valor > 500 : #limite de saque 500
+            print ('Limite de saque R$ 500 , digite outro valor : ')
+        elif contador_transacoes >LIMITE_SAQUES : #Limite 3 saques por dia
+            print ('Só  e permitido fazer 3 saques por dia.')
+        else:
+            saldo = saldo - valor 
+            extrato [f'Saque {contador_transacoes}'] = f'- R$ {valor:.2f}'
+            contador_transacoes+=1 #adiciona quantas transações foram efetuadas para cada operação
+            #extrato.append(f"Saque: +R$ {valor:.2f}")  # Adiciona ao extrato
+            print(f"O Saque de R$ {valor:.2f} foi realizado com sucesso!")
+            print(f"Saldo atualizado: R$ {saldo:.2f}")
+            
+    except ValueError : # se ocorrer algum erro  
+            print("Erro: Digite um número válido.")
+
+
+
+
+while True :
+
+    exibir_menu()
+    escolha = int(input('Escolha uma opção : '))
+    if escolha == 1 :
+            print ()
+            deposito ()
+
+    elif escolha == 2 :
+    
+            print ()
+            sacar ()
+    
+    elif escolha == 3 :
+        # print ()
+        # exibir_extrato()
+        print (f'Exibir extrato : ' , extrato)
+        print (f'Exibir extrato : {saldo:.2f}' )
+    elif escolha == 4 : 
+        print ('Saindo ..... Obrigado por usar o nosso  banco ,  Tenha uma  Boa tarde !') 
+    else : 
+        print ('Opção invalida, tente novamente.')
+        
+
+
+
